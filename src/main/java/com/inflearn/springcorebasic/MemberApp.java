93f8550a689a0787d3b929af4fa6a7ad43e3fbfc
jimin.baek <jimin.baek@navercorp.com>
2022-04-22 01:43:16 +0900

    member domain test

---
+++ src/main/java/com/inflearn/springcorebasic/MemberApp.java
@@ -0,0 +1,18 @@
+package com.inflearn.springcorebasic;
+
+import com.inflearn.springcorebasic.member.Grade;
+import com.inflearn.springcorebasic.member.Member;
+import com.inflearn.springcorebasic.member.MemberService;
+import com.inflearn.springcorebasic.member.MemberServiceImpl;
+
+public class MemberApp {
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Member findMember = memberService.findMember(1L);
+		System.out.println("new member = " + member.getName());
+		System.out.println("find member = " + findMember.getName());
+	}
+}
