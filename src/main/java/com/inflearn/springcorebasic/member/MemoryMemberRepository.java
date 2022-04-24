bb267a5cb6404ceb3ee48b24fc17cf1e84ac09c9
jimin.baek <jimin.baek@navercorp.com>
2022-04-24 23:52:54 +0900

    remove unused import

--- src/main/java/com/inflearn/springcorebasic/member/MemoryMemberRepository.java
+++ src/main/java/com/inflearn/springcorebasic/member/MemoryMemberRepository.java
@@ -2,7 +2,6 @@ package com.inflearn.springcorebasic.member;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryMemberRepository implements MemberRepository {
 
