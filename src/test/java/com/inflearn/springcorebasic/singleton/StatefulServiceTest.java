4082a80e01cb3baae0374dc119764d9c14e103bd
jimin.baek <jimin.baek@navercorp.com>
2022-07-03 16:21:11 +0900

    singleton problem solve test

--- src/test/java/com/inflearn/springcorebasic/singleton/StatefulServiceTest.java
+++ src/test/java/com/inflearn/springcorebasic/singleton/StatefulServiceTest.java
@@ -16,16 +16,18 @@ class StatefulServiceTest {
 		StatefulService statefulService2 = applicationContext.getBean(StatefulService.class);
 
 		// Thread A : A 사용자가 10000원 주문
-		statefulService1.order("userA", 10000);
+		int userAPrice = statefulService1.order("userA", 10000);
 
 		// Thread B : B 사용자가 20000원 주문
-		statefulService2.order("userB", 20000);
+		int userBPrice = statefulService2.order("userB", 20000);
 
 		// Thread A : A 사용자가 주문 금액 조회
-		int price = statefulService1.getPrice();
-		System.out.println("price = " + price);
+		// int price = statefulService1.getPrice();
+		// System.out.println("price = " + price);
 
-		assertThat(statefulService1.getPrice()).isEqualTo(20000);
+		// assertThat(statefulService1.getPrice()).isEqualTo(20000);
+		assertThat(userAPrice).isEqualTo(10000);
+		assertThat(userBPrice).isEqualTo(20000);
 	}
 
 	static class TestConfig {
